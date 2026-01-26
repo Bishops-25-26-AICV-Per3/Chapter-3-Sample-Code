6ee4fcd447107793182602f37c9ef303df404936
TBSDrJ (Marcus Jaiclin) <marcus.jaiclin@bishops.com>
2026-01-26 10:28:16 -0800

    First CNN per 5

---
+++ first_nn_tf_per_5.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+INPUT_SHAPE = (224, 224, 3)
+
+class Model():
+    """Represent the CNN as an object."""
+    def __init__(self, input_shape: (int, int, int)):
+        """input_shape is expected to be channels last."""
+        self.model = tf.keras.Sequential()
+        # Your first layer of any type requires the input_shape keyword arg.
+        self.model.add(tf.keras.layers.ZeroPadding2D(
+            ((2, 1), (2, 1)),
+            input_shape = input_shape,
+        ))
+        self.model.add(tf.keras.layers.Conv2D(
+            filters = 48,
+            kernel_size = 11,
+            strides = 4,
+            activation = "relu",
+        ))
+
+def main():
+    model = Model(INPUT_SHAPE)
+    model.model.summary()
+
+if __name__ == "__main__":
+    main()
